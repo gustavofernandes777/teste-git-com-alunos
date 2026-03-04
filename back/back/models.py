a356d309ddb99764a4d0f929854a03a6d531b8d1
lupereira294 <lucyanaana8@gmail.com>
2026-03-04 15:05:18 -0300

    finalizando models

--- back/back/models.py
+++ back/back/models.py
@@ -9,7 +9,7 @@ class Usuario(Base):
     id = Column(Integer, primary_key=True)
     nome = Column (String)
     email = Column (String)
-    animais= relationship("Animais", back_populates="usuario")
+    animais= relationship("Animais", back_populates="usuarios")
 
 #animais
 
@@ -21,14 +21,23 @@ class Animais(Base):
     especie = Column(String)
 
     usuario = relationship("Usuario", back_populates="animais")
+    consulta= relationship("Consulta", back_populates="animais")
 
 
 
-class Veterinário(Base):
-    __tablename__ = "Veterinários"
+class Veterinario(Base):
+    __tablename__ = "veterinarios"
     id = Column(Integer, primary_key=True)
     nome = Column (String)
     email = Column (String)
     telefone = Column (String)
+    consulta= relationship("Consulta", back_populates="veterinarios")
 
-    
+class Consulta(Base):
+    __tablename__ = "consultas"
+    id = Column(Integer, primary_key=True)
+    animal_id = Column(Integer, ForeignKey("animais.id"))
+    veterinario_id = Column(Integer, ForeignKey("veterinarios.id"))
+    data = Column(String)
+    veterinario = relationship("Veterinario", back_populates="consultas")
+    animais = relationship("Animais", back_populates="consultas")
